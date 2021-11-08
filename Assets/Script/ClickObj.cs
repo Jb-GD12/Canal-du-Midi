560bb1933a2d5bf5725fb38c6572965710d8ed61
Jb-JV21 <59962589+Jb-GD12@users.noreply.github.com>
2021-11-08 11:48:41 +0100

    essaie rotation

---
+++ Assets/Script/ClickObj.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickObj : MonoBehaviour
+{
+    [SerializeField] [Range(0, 10)] [Tooltip("Force d'impulsion de la tuile")] private float m_impulseForce;
+    [SerializeField] [Tooltip("Vitesse de rotation de la tuile")] private float m_rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                RotateObj rotateScript = hitInfo.collider.GetComponent<RotateObj>();
+                if (rotateScript != null)
+                {
+                    rotateScript.Rotation(m_impulseForce);
+                }
+            }
+        }
+    }
+}
